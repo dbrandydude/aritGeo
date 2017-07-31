be1c72a740e30869c8de555d04c75f33ae45f03b
Dbrandy <dbrandydude@gmail.com>
2017-07-31 21:58:06 +0100

    Refactor code - Fix bug for test 10

--- src/aritGeo.js
+++ src/aritGeo.js
@@ -2,28 +2,29 @@
 
 /**
  * aritGeo function
+ *
  * @param  {array}          {int}
  * @return {int || sting}  {'Arithmetic' || 'Geometric' || 0 || -1}
- *
  */
 const aritGeo = (list) => {
-    if (!Array.isArray(list)) return 'Argument must be an array';
-    if (list.length === 0) return 0;
+    // verify that list is an array or not empty
+    if (!Array.isArray(list) || list.length === 0) return 0;
 
-    let result; // result for checks
+    let result, // result for checks
+        aritConst = list[1] - list[0], // arithmetic common ratio
+        geoConst = list[1] / list[0]; // geometric common ratio
 
-    let aritConst = list[1] - list[0]; // arithmetic ratio
-    let geoConst = list[1] / list[0]; // geometric ratio
-
-    // Loop through list and perform checks
-    for (let i = 0; i < list.length - 1; i++) {
-        if (list[i] + aritConst === list[i + 1]) { // check for arithmetic
-            result = 'Arithmetic';
-        } else if (list[i] * geoConst === list[i + 1]) { // check for geometric
-            result = 'Geometric';
-        } else result = -1; // check for neither arithmetic/geometric
+    /* Loop through list and perform check for arithmetic progression */
+    for (let i = 1; i < list.length; i++) {
+        if (list[0] + aritConst * i === list[i]) result = 'Arithmetic';
+        else { // if arithmetic fails, check for geometric
+            for (let i = 0; i < list.length - 1; i++) {
+                if (list[i] * geoConst === list[i + 1]) result = 'Geometric';
+                else result = -1; // if both fails
+            }
+        }
     }
-    // return result
+
     return result;
 };
 
