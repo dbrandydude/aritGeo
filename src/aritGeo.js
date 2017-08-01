38510ad6588a172d4145c613b58f44e9d5a8c1af
Dbrandy <dbrandydude@gmail.com>
2017-08-01 22:26:42 +0100

    Clean up code - Remove comment where not necessary. modified:   src/aritGeo.js

--- src/aritGeo.js
+++ src/aritGeo.js
@@ -10,17 +10,17 @@ const aritGeo = (list) => {
     // verify that list is an array or not empty
     if (!Array.isArray(list) || list.length === 0) return 0;
 
-    let result, // result for checks
-        aritConst = list[1] - list[0], // arithmetic common ratio
-        geoConst = list[1] / list[0]; // geometric common ratio
+    let result,
+        aritConst = list[1] - list[0],
+        geoConst = list[1] / list[0];
 
-    /* Loop through list and perform check for arithmetic progression */
+    /* loop through list and perform checks */
     for (let i = 1; i < list.length; i++) {
         if (list[0] + aritConst * i === list[i]) result = 'Arithmetic';
-        else { // if arithmetic fails, check for geometric
+        else {
             for (let i = 0; i < list.length - 1; i++) {
                 if (list[i] * geoConst === list[i + 1]) result = 'Geometric';
-                else result = -1; // if both fails
+                else result = -1;
             }
         }
     }
